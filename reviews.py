4789bc69aad55e53719b29942c74b7a02860dada
parksontyc <parkson.tyc.chen@gmail.com>
2021-02-10 21:55:32 +0800

    'add_count'

--- reviews.py
+++ reviews.py
@@ -7,37 +7,72 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data)) #每讀1000筆印一次
-print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
 print(data[0])
-print('-----------------------------')
-print(data[1])
 
-sum_len = 0
-for i in data:
-	sum_len = sum_len + len(i)
-	#print(sum_len)
-avg = sum_len / len(data)
-print(avg)
+wc = {}
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 
 
-new = []
-for i in data:
-	if len(i) <= 100:
-		new.append(i)
-		#print(len(new))
-print('共有', len(new), '筆資料小於100')
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
 
-word = []
-for i in data:
-	if 'good' in i:
-		word.append(i)
-print('總共有', len(word), '筆資料裡面含有good字串')
+while True:
+	word = input('請問你要查什麼字：')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為', wc[word], '次')
+	else:
+		print('沒有這個字')
 
-word = [i for i in data if "good" in i]
-print('總共有', len(word), '筆資料裡面含有good字串')
+print('感謝使用本功能！')
 
-word = [1 for i in data if 'good' in i] #裝進1
-print('總共有', len(word), '筆資料裡面含有good字串')
-print(word)
 
-bad = ["bad" in i for i in data] #boolean
-print(bad)
+
+
+
+
+
+
+
+# print(data[0])
+# print('-----------------------------')
+# print(data[1])
+
+# sum_len = 0
+# for i in data:
+# 	sum_len = sum_len + len(i)
+# 	#print(sum_len)
+# avg = sum_len / len(data)
+# print(avg)
+
+# new = []
+# for i in data:
+# 	if len(i) <= 100:
+# 		new.append(i)
+# 		#print(len(new))
+# print('共有', len(new), '筆資料小於100')
+
+# word = []
+# for i in data:
+# 	if 'good' in i:
+# 		word.append(i)
+# print('總共有', len(word), '筆資料裡面含有good字串')
+
+# word = [i for i in data if "good" in i]
+# print('總共有', len(word), '筆資料裡面含有good字串')
+
+# word = [1 for i in data if 'good' in i] #裝進1
+# print('總共有', len(word), '筆資料裡面含有good字串')
+# print(word)
+
+# bad = ["bad" in i for i in data] #boolean
+# print(bad)
